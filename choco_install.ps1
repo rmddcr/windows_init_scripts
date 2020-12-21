d4b2f67ca996a9dfea9c8b322c65678f87c2a78e
Damitha Ranasinghe <rmddcr@gmail.com>
2020-12-21 07:50:31 +0530

    added set-default funtion

--- choco_install.ps1
+++ choco_install.ps1
@@ -1,1 +1,8 @@
 choco install -y qbittorrent google-backup-and-sync 7zip googlechrome
+function Set-DefaultBrowser {
+Add-Type -AssemblyName 'System.Windows.Forms'
+Start-Process $env:windir\system32\control.exe -LoadUserProfile -Wait -ArgumentList '/name Microsoft.DefaultPrograms /page pageDefaultProgram\pageAdvancedSettings?pszAppName=Firefox-308046B0AF4A39CB'
+Sleep 10
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB} {ENTER}{ENTER} ")
+}
+Set-DefaultBrowser;
